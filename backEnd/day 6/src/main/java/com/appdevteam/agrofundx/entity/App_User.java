8ea4df6803face51dd5da555d2216c34383f50a1
Aaditya0110 <113664144+Aaditya0110@users.noreply.github.com>
2024-02-27 19:02:23 +0530

    Add files via upload

---
+++ backEnd/day 6/src/main/java/com/appdevteam/agrofundx/entity/App_User.java
@@ -0,0 +1,20 @@
+package com.appdevteam.agrofundx.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class App_User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private String email;
+    private int phoneNo;
+}
